3de49b1a76ba8de24d8c66e4d99db0f97912e6a3
Parker <meet.parker@foxmail.com>
2020-09-13 22:04:13 +0800

    更改打印样式

--- opsli-web/src/main/java/org/opsli/OpsliApplication.java
+++ opsli-web/src/main/java/org/opsli/OpsliApplication.java
@@ -1,10 +1,8 @@
 package org.opsli;
 
-import org.opsli.common.print.StartPrint;
+import org.opsli.general.StartPrint;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
---
+++ opsli-web/src/main/java/org/opsli/general/StartPrint.java
@@ -0,0 +1,47 @@
+package org.opsli.general;
+
+import cn.hutool.core.thread.ThreadUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @BelongsProject: opsli-boot
+ * @BelongsPackage: org.opsli.core.print
+ * @Author: Parker
+ * @CreateTime: 2020-09-12 17:54
+ * @Description: 打印启动日志
+ */
+@Slf4j
+public enum StartPrint {
+
+    /** 实例对象 */
+    INSTANCE;
+
+    /**
+     * 打印启动日志
+     */
+    public void print(Environment env){
+        // 睡一秒打印
+        ThreadUtil.sleep(1, TimeUnit.SECONDS);
+        String ip = "localhost";
+        try {
+            ip = InetAddress.getLocalHost().getHostAddress();
+        }catch (UnknownHostException e){
+            log.error("网卡IP 获取失败");
+        }
+        String serverPort = env.getProperty("server.port");
+        String contextPath = env.getProperty("server.servlet.context-path");
+        StringBuilder printStr = new StringBuilder();
+        printStr.append("\n----------------------------------------------------------\n")
+                .append("Opsli-Boot 框架已启动! 相关URLs:\n")
+                .append("项目地址: \t\thttp://" + ip + ":" + serverPort + contextPath + "/\n")
+                .append("Doc文档: \t\thttp://" + ip + ":" + serverPort + contextPath + "/doc.html\n")
+                .append("----------------------------------------------------------\n");
+        log.info(printStr.toString());
+    }
+
+}
