70d11f7c259ba4ee1296c9810d77836095ec5d91
Parker <meet.parker@foxmail.com>
2020-09-18 16:25:23 +0800

    Api Controller 统一接口

---
+++ opsli-common/src/main/java/org/opsli/common/annotation/ApiRestController.java
@@ -0,0 +1,36 @@
+package org.opsli.common.annotation;
+
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.lang.annotation.*;
+
+/**
+ * controller层统一使用该注解
+ * @author C西
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@RestController
+@RequestMapping
+public @interface ApiRestController {
+    /**
+     * Alias for {@link RequestMapping#name}.
+     */
+    @AliasFor(annotation = RequestMapping.class)
+    String name() default "";
+
+    /**
+     * Alias for {@link RequestMapping#value}.
+     */
+    @AliasFor(annotation = RequestMapping.class)
+    String[] value() default {};
+
+    /**
+     * Alias for {@link RequestMapping#path}.
+     */
+    @AliasFor(annotation = RequestMapping.class)
+    String[] path() default {};
+}
