72cd13c3f070c21294175e992142e349c9822b73
Parker <meet.parker@foxmail.com>
2020-09-18 16:27:00 +0800

    增加广播ID ， 对于自身广播不做处理

--- opsli-plugins/opsli-plugins-redis/src/main/java/org/opsli/plugins/redis/pushsub/entity/BaseSubMessage.java
+++ opsli-plugins/opsli-plugins-redis/src/main/java/org/opsli/plugins/redis/pushsub/entity/BaseSubMessage.java
@@ -3,12 +3,23 @@ package org.opsli.plugins.redis.pushsub.entity;
 import com.alibaba.fastjson.JSONObject;
 import lombok.Data;
 import lombok.experimental.Accessors;
+import org.opsli.common.enums.SystemInfo;
 
+/**
+ * @author parker
+ * @date 2020-09-13
+ *
+ * 广播消息时，不会处理 自身的数据，如果要处理请预先处理
+ *
+ * 防止广播数据处理到自身时出现延迟问题
+ *
+ */
 @Data
 @Accessors(chain = true)
 public class BaseSubMessage implements RedisPushSubMessage{
 
     public static final String BASE_TYPE = "TYPE";
+    public static final String BASE_ID = "IDENTIFIER";
 
     /** 发布订阅频道名称 */
     protected String channel;
@@ -27,6 +38,7 @@ public class BaseSubMessage implements RedisPushSubMessage{
             return;
         }
         jsonObj.put(BASE_TYPE, type);
+        jsonObj.put(BASE_ID, SystemInfo.INSTANCE.getSystemID());
         this.json = jsonObj.toString();
         this.channel = channel;
     }
