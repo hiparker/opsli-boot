b35e6f956d94c3ff17739e640ce81f39e4660772
Parker <meet.parker@foxmail.com>
2021-01-05 21:12:23 +0800

    限流器测试

---
+++ opsli-modulars/opsli-modulars-system/src/test/java/LimiterTest.java
@@ -0,0 +1,29 @@
+import cn.hutool.http.HttpUtil;
+import com.google.common.collect.Maps;
+import org.junit.Test;
+import org.opsli.common.thread.refuse.AsyncProcessQueueReFuse;
+
+import java.util.Map;
+
+/**
+ * @BelongsProject: think-bboss-parent
+ * @BelongsPackage: PACKAGE_NAME
+ * @Author: Parker
+ * @CreateTime: 2021-01-05 17:17
+ * @Description: 限流器测试
+ */
+public class LimiterTest {
+
+    @Test
+    public void test(){
+        for (int i = 0; i < 1000; i++) {
+            AsyncProcessQueueReFuse.execute(()->{
+                Map<String,Object> map = Maps.newHashMap();
+                map.put("username","demo");
+                String ret = HttpUtil.get("http://127.0.0.1:9000/think-bboss-thd/sys/slipCount", map);
+                System.out.println(ret);
+            });
+        }
+    }
+
+}
