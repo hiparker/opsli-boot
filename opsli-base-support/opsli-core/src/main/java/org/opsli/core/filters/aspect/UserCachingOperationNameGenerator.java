4e2965cedac78103160bbb5fa6ca225dcd86fa3c
hiparker <meet.parker@foxmail.com>
2021-05-19 14:13:17 +0800

    优化缩进

--- opsli-base-support/opsli-core/src/main/java/org/opsli/core/filters/aspect/UserCachingOperationNameGenerator.java
+++ opsli-base-support/opsli-core/src/main/java/org/opsli/core/filters/aspect/UserCachingOperationNameGenerator.java
@@ -38,31 +38,30 @@ import static com.google.common.collect.Maps.newHashMap;
 @Aspect
 public class UserCachingOperationNameGenerator {
 
-
-  private final Map<String, Integer> generated = Maps.newHashMap();
-
-
-  @Pointcut("execution(* springfox.documentation.spring.web.readers.operation.CachingOperationNameGenerator.startingWith(String))")
-  public void c() {
-  }
+    private final Map<String, Integer> generated = Maps.newHashMap();
 
 
-  @Around("c()")
-  public Object a(ProceedingJoinPoint point) {
-    Object[] args = point.getArgs();
-    return startingWith(String.valueOf(args[0]));
-  }
-
-  private String startingWith(String prefix) {
-    if (generated.containsKey(prefix)) {
-      generated.put(prefix, generated.get(prefix) + 1);
-      //log.warn("组件中存在相同的方法名称，自动生成组件方法唯一名称进行替换: {}", nextUniqueOperationName);
-      return String.format("%s_%s", prefix, generated.get(prefix));
-    } else {
-      generated.put(prefix, 0);
-      return prefix;
+    @Pointcut("execution(* springfox.documentation.spring.web.readers.operation.CachingOperationNameGenerator.startingWith(String))")
+    public void c() {
     }
-  }
+
+
+    @Around("c()")
+    public Object a(ProceedingJoinPoint point) {
+        Object[] args = point.getArgs();
+        return startingWith(String.valueOf(args[0]));
+    }
+
+    private String startingWith(String prefix) {
+        if (generated.containsKey(prefix)) {
+            generated.put(prefix, generated.get(prefix) + 1);
+            //log.warn("组件中存在相同的方法名称，自动生成组件方法唯一名称进行替换: {}", nextUniqueOperationName);
+            return String.format("%s_%s", prefix, generated.get(prefix));
+        } else {
+            generated.put(prefix, 0);
+            return prefix;
+        }
+    }
 }
 
 
