39bf776e46ffccb773fee57a2c463ea6f1341f23
Carina <meet.carina@foxmail.com>
2021-12-14 10:22:04 +0800

    fix: 修复组织机构 租户下编号重复BUG

--- opsli-modulars/opsli-modulars-system/src/main/java/org/opsli/modulars/system/org/service/impl/SysOrgServiceImpl.java
+++ opsli-modulars/opsli-modulars-system/src/main/java/org/opsli/modulars/system/org/service/impl/SysOrgServiceImpl.java
@@ -349,6 +349,7 @@ public class SysOrgServiceImpl extends CrudServiceImpl<SysOrgMapper, SysOrg, Sys
             return false;
         }
         QueryWrapper<SysOrg> wrapper = new QueryWrapper<>();
+        wrapper.eq(FieldUtil.humpToUnderline(MyBatisConstants.FIELD_TENANT), model.getTenantId());
         wrapper.eq("org_code", model.getOrgCode());
 
         // 重复校验排除自身
